4d40a3ba0e91351b3c2350b9d57dca3e41387940
malang <malangself@gmail.com>
2024-08-17 09:24:56 +0800

    show the img and title

---
+++ main.js
@@ -0,0 +1,46 @@
+const apiKey = "098f1ccda15145ff914288993bfbbd2c"
+const cardChild = document.getElementById("card-container")
+
+async function fetchResponse() {
+    try {
+        const apiUrl = `https://newsapi.org/v2/top-headlines?pageSize=10&sources=techcrunch&apiKey=${apiKey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return  data.articles;
+    } catch (error) {
+        console.log("haven't whis data");
+        return []
+    }
+}
+
+function disPlayBlogs(articles) {
+    cardChild.innerHTML = "";
+    articles.forEach((article) => {
+        const blogCard = document.createElement("div")
+        blogCard.classList.add("card-block")
+        const img = document.createElement("img")
+        img.src = article.urlToImage
+        img.alt = article.title
+        const title = document.createElement("h2")
+        title.textContent = article.title
+        const description = document.createElement("p")
+        description.textContent = article.description
+
+        blogCard.appendChild(img)
+        blogCard.appendChild(title)
+        blogCard.appendChild(description)
+
+        cardChild.appendChild(blogCard)
+    });
+}
+
+(async () => {
+    console.log("object");
+    try {
+        const articles = await fetchResponse();
+        console.log("articles is >>",articles);
+        disPlayBlogs(articles)
+    } catch (error) {
+        console.log("bug by return data >>", error);
+    }
+})();
